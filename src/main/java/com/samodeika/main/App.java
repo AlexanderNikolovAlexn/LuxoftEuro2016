4d61917afe85d83af2f090fb2ef50cebf1b66f6b
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2016-06-13 10:56:26 +0300

    initial commit for empty repo

---
+++ src/main/java/com/samodeika/main/App.java
@@ -0,0 +1,12 @@
+package com.samodeika.main;
+
+/**
+ * Created by Alexander Nikolov on 13.6.2016 ?..
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Run main!");
+    }
+
+}
